3645c8f915b32d49defb5edd8927b4120f3223ed
Trung <truongcongtrung2709@gmail.com>
2022-09-21 00:31:17 +0700

    Done

---
+++ .history/controllers/cart_20220920204634.js
@@ -0,0 +1,152 @@
+getProducts();
+let label = dom("#label");
+let shoppingCart = dom("#shopping-cart");
+let products = [];
+let cart = JSON.parse(localStorage.getItem("data")) || [];
+function calcCart() {
+  let cartTotal = dom("#cart-amount");
+  cartTotal.innerHTML = cart
+    .map((cart) => cart.item)
+    .reduce((a, b) => a + b, 0);
+}
+calcCart();
+
+function getProducts() {
+  apiGetProduct()
+    .then((response) => {
+      console.log("API products:", response.data);
+      let search = response.data.find((x) => x.id === cart.id);
+      products = response.data.map((product) => {
+        return new Product(
+          product.id,
+          product.name,
+          product.price,
+          product.screen,
+          product.backCamera,
+          product.frontCamera,
+          product.img,
+          product.desc,
+          product.type
+        );
+      });
+      displayCartProducts();
+      totalAmount();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+console.log(cart);
+function displayCartProducts() {
+  if (cart.length !== 0) {
+    return (shoppingCart.innerHTML = cart.map((productCart) => {
+      let { id, item } = productCart;
+      let search = products.find((product) => product.id === id) || [];
+      return `     
+      <div class="cart-item">
+        <img width="100px" src="${search.img}" alt="" />
+        <div class="title-price-x">
+        <h4 class="title-price">
+        <p>${search.name}</p>
+        <p class="cart-item-price">$${search.price}</p>
+        </h4>
+        <i onclick="removeItem(${id})" class="fa-thin fa-x"></i>
+        </div>
+        <div class="buttons" id="btn-add">
+          <i onclick="decrementCart(${id})" class="fa-solid fa-angle-left" data-type="subtract"></i>
+          <div id=${id} class="quantity">${item}</div>
+          <i onclick="incrementCart(${id})" class="fa-solid fa-angle-right" data-type="add"></i>
+          </div>
+        <h3>$${item * search.price}</h3>
+      </div>
+      `;
+    }));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2 class="text-center text-white">Cart is empty</h2>
+    <a href="index.html">
+    <button class="HomeBtn text-center btn-success">Return to Home</button>
+    </a>
+    `;
+  }
+}
+//=================================================
+function dom(selector) {
+  return document.querySelector(selector);
+}
+function decrementCart(id) {
+  let selectedItem = id;
+  let search = cart.find((item) => item.id === selectedItem);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  updateCart(selectedItem);
+  cart = cart.filter((product) => product.item !== 0);
+  displayCartProducts();
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+
+function incrementCart(id) {
+  let selectedItem = id;
+  let search = cart.find((item) => item.id === selectedItem);
+  if (search === undefined) {
+    cart.push({
+      id: selectedItem,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  displayCartProducts();
+  updateCart(selectedItem);
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+function updateCart(id) {
+  let search = cart.find((item) => item.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calcCart();
+  totalAmount();
+}
+function removeItem(id) {
+  let selectedItem = id;
+  cart = cart.filter((x) => x.id !== selectedItem);
+  displayCartProducts();
+  calcCart();
+  totalAmount();
+
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+function clearCart() {
+  cart = [];
+  displayCartProducts();
+  calcCart();
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+function pay() {
+  cart = [];
+  displayCartProducts();
+  calcCart();
+  localStorage.setItem("data", JSON.stringify(cart));
+  alert("Bạn đã thanh toán thành công");
+}
+function totalAmount() {
+  if (cart.length !== 0) {
+    let amount = cart
+      .map((productCart) => {
+        let { id, item } = productCart;
+        let search = products.find((product) => product.id === id) || [];
+        return item * search.price;
+      })
+      .reduce((sum, item) => sum + item, 0);
+    label.innerHTML = `
+      <h2>Total bill: $${amount}</h2>
+      <button onclick="pay()" class="btn btn-success">Pay</button>
+      <button onclick="clearCart()" class="btn btn-danger">Clear Cart</button>
+      `;
+  } else {
+    return;
+  }
+}
---
+++ .history/controllers/main_20220920222238.js
@@ -0,0 +1,141 @@
+getProducts();
+let products = [];
+let cart = +JSON.parse(localStorage.getItem("data")) || [];
+console.log(cart);
+function getProducts() {
+  apiGetProduct()
+    .then((response) => {
+      console.log("API products:", response.data);
+      products = response.data.map((product) => {
+        return new Product(
+          product.id,
+          product.name,
+          product.price,
+          product.screen,
+          product.backCamera,
+          product.frontCamera,
+          product.img,
+          product.desc,
+          product.type
+        );
+      });
+      display(products);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+//===========================================================
+
+function display(products) {
+  let output = products.reduce((result, product) => {
+    let search = cart.find((item) => item.id === product.id) || [];
+    return (
+      result +
+      `
+      <div class="card">
+
+      <div class="card-top">
+      <div hidden >${product.id}</div>
+      <i class="fa-brands fa-apple"></i>
+      <p class="stock">in-stock</p>
+      </div>
+
+          <div class="card-body">
+            <img class="img-product" src=${product.img}></img>
+          </div>
+          <div class="card-footer">
+          <div class="name-heart">
+          <h4>${product.name}</h4>
+          <button class="heart">
+          <i class="fas fa-heart"></i>
+          </button>
+          </div>
+          <div class="desc-product">
+          <p>${product.desc}, ${product.backCamera}, ${product.frontCamera}</p>
+          </div>
+          <div class="purchase">
+          <p>$${product.price}</p>
+          <div class="buttons" id="btn-add">
+          <i onclick="decrementCart(${
+            product.id
+          })" class="fa-solid fa-angle-left" data-type="subtract"></i>
+          <div id=${product.id} class="quantity">${
+        search.item === undefined ? 0 : search.item
+      }</div>
+          <i onclick="incrementCart(${
+            product.id
+          })" class="fa-solid fa-angle-right" data-type="add"></i>
+          </div>
+          </div>
+          </div>
+        </div>
+      `
+    );
+  }, "");
+
+  dom("#tblproducts").innerHTML = output;
+}
+function dom(selector) {
+  return document.querySelector(selector);
+}
+//=========================DOM=========================
+dom("#select-type").addEventListener("change", (evt) => {
+  let selectValue = evt.target.value;
+  selectValue = selectValue.toLowerCase();
+  if (selectValue === "iphone") {
+    let searchByType = products.filter((product) => {
+      return product.type.toLowerCase() === selectValue;
+    });
+    display(searchByType);
+  } else if (selectValue === "samsung") {
+    searchByType = products.filter((product) => {
+      return product.type.toLowerCase() === selectValue;
+    });
+    display(searchByType);
+  } else {
+    display(products);
+  }
+});
+//====================addCart=================
+function decrementCart(id) {
+  let selectedItem = id;
+  let search = cart.find((item) => item.id === selectedItem);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  updateCart(selectedItem);
+  cart = cart.filter((product) => product.item !== 0);
+
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+
+function incrementCart(id) {
+  let selectedItem = id;
+  let search = cart.find((item) => item.id === selectedItem);
+  if (search === undefined) {
+    cart.push({
+      id: selectedItem,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  updateCart(selectedItem);
+  localStorage.setItem("data", JSON.stringify(cart));
+}
+function updateCart(id) {
+  let search = cart.find((item) => item.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calcCart();
+}
+function calcCart() {
+  let cartTotal = dom("#cart-amount");
+  cartTotal.innerHTML = cart
+    .map((cart) => cart.item)
+    .reduce((a, b) => a + b, 0);
+}
+calcCart();
+//=====================================
